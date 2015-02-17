3c4901a6f07bfcd66308e709e321f36dac9a2c51
Antonio Jimenez <antonio.jimenez2@gmail.com>
2015-02-17 14:54:31 +0100

    Initial Commit

---
+++ MiniShows/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiniShows
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 17/2/15.
+//  Copyright (c) 2015 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiniShows/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiniShows
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 17/2/15.
+//  Copyright (c) 2015 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
