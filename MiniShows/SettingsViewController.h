becda21bb028caad2af85878d56197cb978c32db
Antonio Jimenez <antonio.jimenez2@gmail.com>
2015-02-17 16:44:44 +0100

    Portrait settings ok

---
+++ MiniShows/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  MiniShows
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 17/2/15.
+//  Copyright (c) 2015 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
